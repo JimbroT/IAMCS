6c1f7329fd13726ba65ef8f0e0e6a7729a09cb23
Ganning Xu <ganningxu@gmail.com>
2022-02-18 20:59:57 -0500

    add landing

---
+++ components/DetailsTable.jsx
@@ -0,0 +1,74 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Box, SimpleGrid, Heading, Text, Img, Container
+} from '@chakra-ui/react'
+
+export const DetailsTable = () => {
+    return (
+        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={4} alignItems='center' justifyContent='center'>
+            <Box >
+                <Heading color='white' fontSize='5xl'>
+                    <Text as='span' color='#F1C232'>2022</Text> Summit Details:
+                </Heading>
+                <Table variant='unstyled' mt='4'>
+                    <Tbody p={0} m={0}>
+                        <Tr alignItems='center'>
+                            <Td p={0}>
+                                <Text color='#F1C232' fontSize='3xl' fontWeight='bold'>
+                                    When:
+                                </Text>
+
+                            </Td>
+                            <Td lineHeight='6'>
+                                <Text as='span' color='white' fontSize='2xl' fontWeight='normal' >The Friday Institute for Educational Innovation </Text>
+                                <Text color='gray.300' fontSize='lg'>
+                                    1890 Main Campus Dr, Raleigh, NC 27606
+                                </Text>
+                            </Td>
+                        </Tr>
+                        <Tr alignItems='center'>
+                            <Td p={0}>
+                                <Text color='#F1C232' fontSize='3xl' fontWeight='semibold'>
+                                    When:
+                                </Text>
+
+                            </Td>
+                            <Td lineHeight='6'>
+                                <Text as='span' color='white' fontSize='2xl' fontWeight='normal'>April 30th 2022 </Text>
+                                <Text color='gray.300' fontSize='lg'>
+                                    10am - 5:30pm
+                                </Text>
+                            </Td>
+                        </Tr>
+
+                        <Tr alignItems='center'>
+                            <Td p={0}>
+                                <Text color='#F1C232' fontSize='3xl' fontWeight='semibold'>
+                                    About:
+                                </Text>
+
+                            </Td>
+                            <Td lineHeight='6'>
+                                <Text as='span' color='white' fontSize='2xl' fontWeight='normal'>This event is aimed at exposing students, with a focus on girls, to computer science, ai, STEM, and robotics.
+                                    <Text as='span' color='green.100' fontStyle='italic' fontWeight='bold'> Due to this, participants must be in grades 6th to 12th.</Text></Text>
+                            </Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+
+            </Box>
+
+            <Box>
+                <Img src='/sie.png' mx='auto' maxH='500px' />
+            </Box>
+
+        </SimpleGrid>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/montserrat"
+import "@fontsource/oswald"
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Oswald',
+    body: 'Montserrat',
+  },
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
