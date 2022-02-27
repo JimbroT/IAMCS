6ee599fcd5632c6440cbdea6db25609e627b5ab7
Ganning Xu <ganningxu@gmail.com>
2022-02-27 10:53:29 -0500

    add website workshop component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from '../styles/Home.module.css'
 import { Landing } from '../components/Landing'
 import { DetailsTable } from '../components/DetailsTable'
 import { Box, SimpleGrid, Heading, Text, Img, Container } from '@chakra-ui/react'
+import { NavBar } from '../components/NavBar'
+import { TwoColWithPic } from '../components/TwoColWithPic'
+import { MedSep } from '../components/MedSep'
+import { SmSep } from '../components/SmSep'
 export default function Home() {
   return (
     <>
@@ -15,6 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
+      <NavBar bg='#3B005A' />
       <Landing />
       <Box bgPos='center' bgRepeat='repeat' minH='40vh'
         bgImage="linear-gradient(to top, rgba(0, 0, 0, 0.5), rgb(23, 0, 35)), url('/iamcs_bkg.png') "
@@ -24,6 +29,30 @@ export default function Home() {
           <Box pt={16}>
 
             <DetailsTable />
+            <MedSep />
+            <Box textAlign="center" maxW="700px" mx="auto">
+              <Heading as="h1" size="3xl" color="white" fontWeight="bold" id="workshops">Workshops</Heading>
+            </Box >
+
+            <SmSep />
+
+            <TwoColWithPic
+              img='/VEX Robot Poster.png'
+              title='VEX-ing into the Robo World'
+              desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry' s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+              align='left'
+            />
+
+            <SmSep />
+
+            <TwoColWithPic
+              img='/VEX Robot Poster.png'
+              title='VEX-ing into the Robo World'
+              desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry' s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+              align='right'
+            />
+
+            <SmSep />
 
 
 
